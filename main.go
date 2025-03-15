ddc18c902d5937f5c5255a4ae16639a0ec72311d
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2025-03-15 09:19:33 +0300

    add middleware for count hits

--- main.go
+++ main.go
@@ -1,8 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) requestsCountHandler(w http.ResponseWriter, req *http.Request) {
+	res := fmt.Sprintf("%s%d", "Hits: ", cfg.fileserverHits.Load())
+	w.Write([]byte(res))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Swap(0)
+}
 
 func endpointHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -11,12 +33,15 @@ func endpointHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	var apicfg apiConfig
 	serveMuxplier := http.NewServeMux()
 	server := http.Server{
 		Handler: serveMuxplier,
 		Addr:    ":8080",
 	}
-	serveMuxplier.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serveMuxplier.Handle("/app/", http.StripPrefix("/app", apicfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	serveMuxplier.HandleFunc("/healthz", endpointHandler)
+	serveMuxplier.HandleFunc("/metrics", apicfg.requestsCountHandler)
+	serveMuxplier.HandleFunc("/reset", apicfg.resetHandler)
 	server.ListenAndServe()
 }
